27ad20a48adfa9d4d9d93a8a90d75f07fe3ab77d
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-01-24 16:09:02 -0500

    Functional Profile photo editing

--- src/Contexts/Session/Firebase.tsx
+++ src/Contexts/Session/Firebase.tsx
@@ -119,16 +119,12 @@ export function useAuth() {
 }
 
 // Storage
-export async function upload(file, currentUser, setLoading) {
-  const fileRef = ref(storage, currentUser.uid + ".png");
-
-  // setLoading(true);
+export async function upload(file, currentUser) {
+  console.log(file);
+  const fileRef = ref(storage, currentUser.uid + ".jpeg");
 
   const snapshot = await uploadBytes(fileRef, file);
   const photoURL = await getDownloadURL(fileRef);
 
   updateProfile(currentUser, { photoURL });
-
-  // setLoading(false);
-  alert("Uploaded file!");
 }
--- src/Routes/MyAccount.tsx
+++ src/Routes/MyAccount.tsx
@@ -5,83 +5,157 @@ import Cropper from "react-easy-crop";
 import "react-image-crop/dist/ReactCrop.css";
 import { Button } from "@mui/material";
 import "./MyAccount.css";
-import getCroppedImg from "./CropImage.js";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useError } from "../Contexts/Error/ErrorContext.tsx";
+import Slider from "@mui/material/Slider";
 
 function MyAccount() {
   const currentUser = auth.currentUser;
-
-  const [imageUploaded, setImageUploaded] = useState(false);
+  const { showError } = useError();
+  // State variables
   const [photoURL, setPhotoURL] = useState(currentUser?.photoURL);
   const [photo, setPhoto] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [uploading, setUploading] = useState(false);
 
+  // Cropping state variables
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
   const [zoom, setZoom] = useState(1);
-
   const [croppedImage, setCroppedImage] = useState(null);
 
+  // Effect to update photoURL when currentUser changes
   useEffect(() => {
     if (currentUser?.photoURL) {
       setPhotoURL(currentUser.photoURL);
     }
   }, [currentUser]);
 
-  const onCropComplete = (croppedArea, croppedAreaPixels) => {
-    // console.log(croppedArea, croppedAreaPixels);
+  // Handle crop completion
+  const onCropComplete = (_croppedArea, croppedAreaPixels) => {
     setCroppedAreaPixels(croppedAreaPixels);
   };
 
+  // Handle file selection
   const handleChange = (e) => {
     if (e.target.files[0]) {
       setPhoto(e.target.files[0]);
       setLoading(true);
+      console.log(e.target.files[0]);
     }
   };
 
-  // const handleClick = async () => {
-  //   // Perform cropping here before uploading
-  //   await upload(croppedImage, currentUser, setLoading);
-  //   setImageUploaded(true);
-  //   setLoading(false);
-  // };
+  // Get cropped image asynchronously
+  const getCroppedImg = async () => {
+    try {
+      const croppedImageBlob = await getCroppedImgBlob(
+        photo,
+        croppedAreaPixels
+      );
+      setCroppedImage(croppedImageBlob);
+      return croppedImageBlob;
+    } catch (error) {
+      console.error("Error getting cropped image:", error);
+    }
+  };
 
+  // Get cropped image blob
+  const getCroppedImgBlob = (image, croppedAreaPixels) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = URL.createObjectURL(image);
+
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+
+        const { width, height } = croppedAreaPixels;
+        canvas.width = width;
+        canvas.height = height;
+
+        ctx.drawImage(
+          img,
+          croppedAreaPixels.x,
+          croppedAreaPixels.y,
+          croppedAreaPixels.width,
+          croppedAreaPixels.height,
+          0,
+          0,
+          width,
+          height
+        );
+
+        canvas.toBlob((blob) => {
+          resolve(blob);
+        }, "image/jpeg");
+      };
+
+      img.onerror = (error) => reject(error);
+    });
+  };
+
+  // Handle click for uploading cropped image
   const handleClick = async () => {
-    if (photo) {
+    if (croppedAreaPixels) {
+      const tempCrop = await getCroppedImg();
+      console.log(tempCrop);
+
       try {
-        const croppedImage = await getCroppedImg(photo, croppedAreaPixels, 0);
-        console.log("donee", { croppedImage });
-        setCroppedImage(croppedImage);
+        // Set uploading state to true when upload starts
+        setUploading(true);
 
-        // Upload the cropped image
-        await upload(croppedImage, currentUser, setLoading);
+        // Wait for the upload to complete before updating the UI
+        await upload(tempCrop, currentUser);
 
-        // Update the imageUploaded state
-        setImageUploaded(true);
-        setLoading(false);
-      } catch (e) {
-        console.error(e);
+        // Update photoURL after successful upload
+        const updatedUser = auth.currentUser;
+        setPhotoURL(updatedUser.photoURL);
+      } catch (error) {
+        showError("Error uploading file", error.code);
+        // Handle the error and update the UI accordingly
+      } finally {
+        // Set uploading state to false when upload completes (whether successful or not)
+        setUploading(false);
+
+        // Reset state
+        setCrop({ x: 0, y: 0 });
+        setCroppedAreaPixels(null);
+        setZoom(1);
+        setPhoto(null);
       }
+    } else {
+      // Handle case when no cropping has been performed
+      console.warn("No cropping has been performed.");
     }
   };
 
-  const handleImageLoaded = (image) => {
-    // You can perform additional actions when the image is loaded
-    console.log("Image loaded:", image);
+  // Handle click for cancel button
+  const handleCancel = () => {
+    // Reset state when cancel button is clicked
+    setPhoto(null);
+    setLoading(false);
+    setCrop({ x: 0, y: 0 });
+    setCroppedAreaPixels(null);
+    setZoom(1);
+    setCroppedImage(null);
   };
-
+  // Render component
   return (
     <>
+      {/* File selection button */}
       <Button variant="contained" component="label">
-        Select File
+        Change photo
         <input type="file" hidden onChange={handleChange} />
       </Button>
-      <Button disabled={!loading} onClick={handleClick} variant="outlined">
-        Upload
-      </Button>
 
-      {photo && !imageUploaded && (
+      {/* Cropper and controls */}
+      {photo && (
         <>
+          {uploading && (
+            <div className="overlay">
+              <CircularProgress size={50} />
+            </div>
+          )}
           <div className="crop-container">
             <Cropper
               image={URL.createObjectURL(photo)}
@@ -94,23 +168,29 @@ function MyAccount() {
             />
           </div>
           <div className="controls">
-            <input
-              type="range"
+            <Button onClick={handleCancel} color="secondary" variant="outlined">
+              X
+            </Button>
+
+            <Slider
               value={zoom}
               min={1}
-              max={3}
-              step={0.1}
+              max={4}
+              step={0.05}
               aria-labelledby="Zoom"
-              onChange={(e) => setZoom(e.target.value)}
-              className="zoom-range"
+              onChange={(_e, value) => setZoom(Number(value))}
+              className="zoom-slider"
+              sx={{ margin: "0 20px", maxWidth: "40%" }}
             />
+
             <Button onClick={handleClick} variant="contained" color="primary">
-              Commit and Upload
+              Ok
             </Button>
           </div>
         </>
       )}
 
+      {/* Display user's avatar */}
       <img
         src={
           croppedImage
@@ -118,7 +198,7 @@ function MyAccount() {
             : photoURL || noAvatar
         }
         alt="Avatar"
-        className="avatarNewBigImg"
+        className="avatarBigImg"
       />
     </>
   );
