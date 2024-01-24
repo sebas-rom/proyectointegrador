768cff8650d74f913ad7c5ea51b7f810f64d5c90
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-01-24 16:03:02 -0500

    Updates

---
+++ assets/LandingPage-jEZQw1ER.js
@@ -0,0 +1,1 @@
+import{ae as o,j as n,a5 as i}from"./index-H2AXzJ-N.js";function r(){const a=o(),t=()=>{a("/login")},e=()=>{a("/signup")};return n.jsxs(n.Fragment,{children:[n.jsx(i,{variant:"contained",onClick:t,children:"Login"}),n.jsx(i,{variant:"contained",onClick:e,children:"SignUp"})]})}export{r as default};
