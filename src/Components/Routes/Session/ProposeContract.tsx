fa516556f932bbbd42b0b025d7f43e7ef4156b54
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-03-17 19:54:01 -0500

    ui

--- src/Components/Routes/Session/ProposeContract.tsx
+++ src/Components/Routes/Session/ProposeContract.tsx
@@ -1,4 +1,11 @@
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
 
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
@@ -8,6 +15,11 @@ import {
   Box,
   Button,
   Container,
+  Divider,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
   Paper,
   Stack,
   TextField,
@@ -18,14 +30,14 @@ import Grid from "@mui/material/Unstable_Grid2"; // Grid version 2
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import AddIcon from "@mui/icons-material/Add";
 function ProposeContract() {
+  const navigate = useNavigate();
+
   const { contractId } = useParams();
   const { setLoading } = useLoading();
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [toUserName, setToUserName] = useState(null);
   const [toUserPhotoUrl, setToUserPhotoUrl] = useState(null);
-
-  // State for milestones
   const [milestones, setMilestones] = useState([
     {
       title: "",
@@ -34,12 +46,10 @@ function ProposeContract() {
     },
   ]);
 
-  // Handle adding a new milestone
   const handleAddMilestone = () => {
     setMilestones([...milestones, { title: "", amount: "", dueDate: "" }]);
   };
 
-  // Handle deleting a milestone
   const handleDeleteMilestone = (index) => {
     if (milestones.length > 1) {
       setMilestones((prevMilestones) =>
@@ -48,7 +58,6 @@ function ProposeContract() {
     }
   };
 
-  const navigate = useNavigate();
   useEffect(() => {
     setLoading(true);
     console.log(contractId);
@@ -84,13 +93,22 @@ function ProposeContract() {
     };
     getContract();
   }, []);
+
+  const handleCancel = async () => {
+    try {
+      setLoading(true);
+      await deleteDoc(doc(db, "contracts", contractId));
+    } finally {
+      navigate(-1);
+      setLoading(false);
+    }
+  };
+
   const handleUpdateContract = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const uid = auth.currentUser.uid; // Assuming you have the current user's UID
-      const userDocRef = doc(db, "users", uid); // Create a reference directly to the user's document
-
+      const userDocRef = doc(db, "contracts", contractId); // Create a reference directly to the user's document
       // Check if the user’s document exists
       const docSnapshot = await getDoc(userDocRef);
       if (docSnapshot.exists()) {
@@ -98,11 +116,18 @@ function ProposeContract() {
         await updateDoc(userDocRef, {
           title: title,
           description: description,
-          milestones: milestones.map((milestone) => ({
-            ...milestone,
-            // Consider formatting dueDate here if needed
-          })),
         });
+        const messagesRef = collection(
+          db,
+          `contracts/${contractId}/milestones`
+        );
+        for (let i = 0; i < milestones.length; i++) {
+          await addDoc(messagesRef, {
+            title: milestones[i].title,
+            ammount: milestones[i].amount,
+            dueDate: milestones[i].dueDate,
+          });
+        }
       }
     } catch (error) {
       console.error("Error updating contract:", error);
@@ -135,7 +160,7 @@ function ProposeContract() {
 
         <Box component="form" onSubmit={handleUpdateContract}>
           <Stack spacing={2}>
-            <Typography variant="h6">Contract Details</Typography>
+            <Typography variant="h5">Contract Details</Typography>
             <TextField
               label="Contract Title"
               fullWidth
@@ -157,132 +182,171 @@ function ProposeContract() {
             <div />
           </Stack>
 
-          <Typography variant="h6">Project Milestones</Typography>
+          <Typography variant="h5">Project Milestones</Typography>
           {milestones.map((milestone, index) => (
-            <>
+            <div key={index}>
+              <Typography variant="h6" sx={{ marginLeft: 2 }}>
+                {index + 1}.
+              </Typography>
               {index == 0 ? (
                 <Grid container columnSpacing={1} sx={{ width: "100%" }}>
                   <Grid xs={12} md={6}>
-                    <TextField
-                      required
-                      label={`Milestone ${index + 1} Title`}
-                      fullWidth
-                      margin="normal"
-                      value={milestone.title}
-                      onChange={(e) =>
-                        setMilestones((prevMilestones) =>
-                          prevMilestones.map((m, i) =>
-                            i === index ? { ...m, title: e.target.value } : m
+                    <FormControl fullWidth margin="normal">
+                      <InputLabel htmlFor={`milestone-${index + 1}-title`}>
+                        Milestone {index + 1} Title
+                      </InputLabel>
+                      <OutlinedInput
+                        required
+                        id={`milestone-${index + 1}-title`}
+                        label="Milestone Title"
+                        value={milestone.title}
+                        onChange={(e) =>
+                          setMilestones((prevMilestones) =>
+                            prevMilestones.map((m, i) =>
+                              i === index ? { ...m, title: e.target.value } : m
+                            )
                           )
-                        )
-                      }
-                    />
+                        }
+                      />
+                    </FormControl>
                   </Grid>
                   <Grid xs={6} md={3}>
-                    <TextField
-                      required
-                      label="Amount"
-                      type="number"
-                      fullWidth
-                      margin="normal"
-                      value={milestone.amount}
-                      onChange={(e) =>
-                        setMilestones((prevMilestones) =>
-                          prevMilestones.map((m, i) =>
-                            i === index ? { ...m, amount: e.target.value } : m
+                    <FormControl fullWidth margin="normal" variant="outlined">
+                      <InputLabel htmlFor={`milestone-${index + 1}-amount`}>
+                        Ammount
+                      </InputLabel>
+                      <OutlinedInput
+                        required
+                        id={`milestone-${index + 1}-amount`}
+                        type="number"
+                        startAdornment={
+                          <InputAdornment position="start">$</InputAdornment>
+                        }
+                        label="Ammount"
+                        value={milestone.amount}
+                        onChange={(e) =>
+                          setMilestones((prevMilestones) =>
+                            prevMilestones.map((m, i) =>
+                              i === index ? { ...m, amount: e.target.value } : m
+                            )
                           )
-                        )
-                      }
-                    />
+                        }
+                      />
+                    </FormControl>
                   </Grid>
                   <Grid xs={6} md={3}>
-                    <TextField
-                      required
-                      type="date"
-                      fullWidth
-                      margin="normal"
-                      value={milestone.dueDate}
-                      onChange={(e) =>
-                        setMilestones((prevMilestones) =>
-                          prevMilestones.map((m, i) =>
-                            i === index ? { ...m, dueDate: e.target.value } : m
+                    <FormControl fullWidth margin="normal" variant="outlined">
+                      <TextField
+                        required
+                        id={`milestone-${index + 1}-dueDate`}
+                        type="date"
+                        placeholder="Due Date"
+                        value={milestone.dueDate}
+                        onChange={(e) =>
+                          setMilestones((prevMilestones) =>
+                            prevMilestones.map((m, i) =>
+                              i === index
+                                ? { ...m, dueDate: e.target.value }
+                                : m
+                            )
                           )
-                        )
-                      }
-                    />
+                        }
+                      />
+                    </FormControl>
                   </Grid>
                 </Grid>
               ) : (
-                <Grid container columnSpacing={1} sx={{ width: "100%" }}>
-                  <Grid xs={12} md={6}>
-                    <TextField
-                      required
-                      label={`Milestone ${index + 1} Title`}
-                      fullWidth
-                      margin="normal"
-                      value={milestone.title}
-                      onChange={(e) =>
-                        setMilestones((prevMilestones) =>
-                          prevMilestones.map((m, i) =>
-                            i === index ? { ...m, title: e.target.value } : m
-                          )
-                        )
-                      }
-                    />
-                  </Grid>
-                  <Grid xs={6} md={3}>
-                    <TextField
-                      required
-                      label="Amount"
-                      type="number"
-                      fullWidth
-                      margin="normal"
-                      value={milestone.amount}
-                      onChange={(e) =>
-                        setMilestones((prevMilestones) =>
-                          prevMilestones.map((m, i) =>
-                            i === index ? { ...m, amount: e.target.value } : m
-                          )
-                        )
-                      }
-                    />
-                  </Grid>
-                  <Grid xs={5} md={2}>
-                    <TextField
-                      required
-                      type="date"
-                      fullWidth
-                      margin="normal"
-                      value={milestone.dueDate}
-                      onChange={(e) =>
-                        setMilestones((prevMilestones) =>
-                          prevMilestones.map((m, i) =>
-                            i === index ? { ...m, dueDate: e.target.value } : m
-                          )
-                        )
-                      }
-                    />
-                  </Grid>
-                  <Grid xs={1}>
-                    <Stack
-                      sx={{
-                        width: "100%",
-                        height: "100%",
-                      }}
-                      alignContent={"center"}
-                      justifyContent={"center"}
-                    >
-                      <Button
-                        onClick={() => handleDeleteMilestone(index)}
-                        color="error"
+                <>
+                  <Grid container columnSpacing={1} sx={{ width: "100%" }}>
+                    <Grid xs={12} md={6}>
+                      <FormControl fullWidth margin="normal">
+                        <InputLabel htmlFor={`milestone-${index + 1}-title`}>
+                          Milestone {index + 1} Title
+                        </InputLabel>
+                        <OutlinedInput
+                          required
+                          id={`milestone-${index + 1}-title`}
+                          label="Milestone Title"
+                          value={milestone.title}
+                          onChange={(e) =>
+                            setMilestones((prevMilestones) =>
+                              prevMilestones.map((m, i) =>
+                                i === index
+                                  ? { ...m, title: e.target.value }
+                                  : m
+                              )
+                            )
+                          }
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid xs={6} md={3}>
+                      <FormControl fullWidth margin="normal" variant="outlined">
+                        <InputLabel htmlFor={`milestone-${index + 1}-amount`}>
+                          Ammount
+                        </InputLabel>
+                        <OutlinedInput
+                          required
+                          id={`milestone-${index + 1}-amount`}
+                          type="number"
+                          startAdornment={
+                            <InputAdornment position="start">$</InputAdornment>
+                          }
+                          label="Ammount"
+                          value={milestone.amount}
+                          onChange={(e) =>
+                            setMilestones((prevMilestones) =>
+                              prevMilestones.map((m, i) =>
+                                i === index
+                                  ? { ...m, amount: e.target.value }
+                                  : m
+                              )
+                            )
+                          }
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid xs={5} md={2}>
+                      <FormControl fullWidth margin="normal" variant="outlined">
+                        <TextField
+                          required
+                          id={`milestone-${index + 1}-dueDate`}
+                          type="date"
+                          placeholder="Due Date"
+                          value={milestone.dueDate}
+                          onChange={(e) =>
+                            setMilestones((prevMilestones) =>
+                              prevMilestones.map((m, i) =>
+                                i === index
+                                  ? { ...m, dueDate: e.target.value }
+                                  : m
+                              )
+                            )
+                          }
+                        />
+                      </FormControl>
+                    </Grid>
+                    <Grid xs={1}>
+                      <Stack
+                        sx={{
+                          width: "100%",
+                          height: "100%",
+                        }}
+                        alignContent={"center"}
+                        justifyContent={"center"}
                       >
-                        <DeleteOutlineIcon />
-                      </Button>
-                    </Stack>
+                        <Button
+                          onClick={() => handleDeleteMilestone(index)}
+                          color="error"
+                        >
+                          <DeleteOutlineIcon />
+                        </Button>
+                      </Stack>
+                    </Grid>
                   </Grid>
-                </Grid>
+                </>
               )}
-            </>
+            </div>
           ))}
 
           <Stack spacing={2} alignItems={"center"}>
@@ -307,7 +371,11 @@ function ProposeContract() {
             <Button variant="contained" color="primary" type="submit">
               Send Proposal
             </Button>
-            <Button variant="outlined" color="error" type="submit">
+            <Button
+              variant="outlined"
+              color="error"
+              onClick={() => handleCancel()}
+            >
               Cancel
             </Button>
           </Stack>
