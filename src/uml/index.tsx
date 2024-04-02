409f6b2bcfb3c9f4558860cf89788742b4ff9e83
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-04-02 13:30:12 -0500

    added database uml

--- src/uml/index.tsx
+++ src/uml/index.tsx
@@ -3,3 +3,9 @@
  * <img src="media://exampleUml.png" alt="alt text">
  */
 export class ExampleUml {}
+
+/**
+ * Here is a diagram illustrating the architecture:
+ * <img src="media://freecudatabase.svg" alt="alt text">
+ */
+export class freecudatabase {}
