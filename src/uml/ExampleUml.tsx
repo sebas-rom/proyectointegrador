13a43f929fca95e921dfb43fc22971741c3c0571
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-04-02 14:30:52 -0500

    docs

---
+++ src/uml/ExampleUml.tsx
@@ -0,0 +1,5 @@
+/**
+ * Here is a diagram illustrating the architecture:
+ * <img src="media://exampleUml.png" alt="alt text">
+ */
+export class ExampleUml {}
---
+++ src/uml/freecudatabase.tsx
@@ -0,0 +1,5 @@
+/**
+ * Here is a diagram illustrating the architecture:
+ * <img src="media://freecudatabase.svg" alt="alt text">
+ */
+export class freecudatabase {}
