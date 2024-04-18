c29973f76b0bf167297aad24c3dea0853f95a075
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-04-18 11:40:57 -0500

    refractor

---
+++ src/Components/Profile/FetchProfileFeedback.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { FEEDBACK_COLLECTION, FeedbackData, auth, db } from "../../Contexts/Session/Firebase";
+import { collectionGroup, getDocs, query, where } from "firebase/firestore";
+import { Stack } from "@mui/material";
+import ProfileFeedback from "./ProfileFeedback";
+interface ProfileFeedbackProps {
+  uid: string;
+}
+const FetchProfileFeedback: React.FC<ProfileFeedbackProps> = ({ uid }) => {
+  const [loading, setLoading] = useState(true);
+  const [reviews, setReviews] = useState<FeedbackData[]>([]);
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      const feedbackRef = collectionGroup(db, FEEDBACK_COLLECTION);
+      const feedbackQuery = query(feedbackRef, where("forUser", "==", auth.currentUser.uid));
+      const querySnapshot = await getDocs(feedbackQuery);
+      querySnapshot.forEach((doc) => {
+        const tempReviews: FeedbackData[] = [];
+        tempReviews.push(doc.data() as FeedbackData);
+        setReviews(tempReviews);
+      });
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Stack>
+      {loading ? (
+        <>loading...</>
+      ) : (
+        <>
+          {reviews.map((review, index) => (
+            <div key={index}>
+              <ProfileFeedback feedbackData={review} />
+            </div>
+          ))}
+        </>
+      )}
+    </Stack>
+  );
+};
+
+export default FetchProfileFeedback;
--- src/Components/Routes/Session/ViewProfile.tsx
+++ src/Components/Routes/Session/ViewProfile.tsx
@@ -1,34 +1,21 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { FEEDBACK_COLLECTION, FeedbackData, UserData, auth, db, getUserData } from "../../../Contexts/Session/Firebase";
+import { UserData, getUserData } from "../../../Contexts/Session/Firebase";
 import CustomContainer from "../../DataDisplay/CustomContainer";
 import { Stack, Typography } from "@mui/material";
 import ColoredAvatar from "../../DataDisplay/ColoredAvatar";
-import { collectionGroup, getDocs, query, where } from "firebase/firestore";
-import { set } from "date-fns";
-import ProfileFeedback from "../../Profile/ProfileFeedback";
+import FetchProfileFeedback from "../../Profile/FetchProfileFeedback";
 
 function ViewProfile() {
   const { profileUID } = useParams();
   const [loading, setLoading] = useState(true);
   const [userData, setUserData] = useState<UserData>(null);
-  const [reviews, setReviews] = useState<FeedbackData[]>([]);
   useEffect(() => {
     setLoading(true);
-    setReviews([]);
     // fetch user data
     const fetchData = async () => {
       const data = (await getUserData(profileUID)) as UserData;
       setUserData(data);
-      const feedbackRef = collectionGroup(db, FEEDBACK_COLLECTION);
-      const feedbackQuery = query(feedbackRef, where("forUser", "==", auth.currentUser.uid));
-      const querySnapshot = await getDocs(feedbackQuery);
-
-      querySnapshot.forEach((doc) => {
-        const tempReviews: FeedbackData[] = [];
-        tempReviews.push(doc.data() as FeedbackData);
-        setReviews(tempReviews);
-      });
       setLoading(false);
     };
     fetchData();
@@ -54,13 +41,7 @@ function ViewProfile() {
           <Typography variant="body1">userData.about</Typography>
           <Typography variant="h6">Skills</Typography>
           <Typography variant="h5">Reviews</Typography>
-          <Stack>
-            {reviews.map((review, index) => (
-              <div key={index}>
-                <ProfileFeedback feedbackData={review} />
-              </div>
-            ))}
-          </Stack>
+          <FetchProfileFeedback uid={profileUID} />
         </Stack>
       )}
     </CustomContainer>
