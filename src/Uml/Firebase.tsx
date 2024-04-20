d839a0b2e27c02b3be54fd9051551623a065104f
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-04-20 12:08:34 -0500

    docs

--- src/Uml/Firebase.tsx
+++ src/Uml/Firebase.tsx
@@ -2,4 +2,9 @@
  * Here is a diagram illustrating the architecture:
  * <img src="media://database.svg" alt="alt text">
  */
-export class freecudatabase {}
+export class database {}
+
+/**
+ * <img src="media://storage.svg" alt="alt text">
+ */
+export class storage {}
--- src/uml/ExampleUml.tsx
+++
@@ -1,5 +0,0 @@
-/**
- * Here is a diagram illustrating the architecture:
- * <img src="media://exampleUml.png" alt="alt text">
- */
-export class ExampleUml {}
