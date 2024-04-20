108e070c47a86401f538468645e7934308a90eab
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-04-20 16:23:58 -0500

    ui

--- src/Components/DataDisplay/CustomPaper.tsx
+++ src/Components/DataDisplay/CustomPaper.tsx
@@ -12,18 +12,18 @@ export interface CustomPaperProps {
 const CustomPaper: React.FC<CustomPaperProps> = ({ sx = {}, children, messagePaper, grayVariant }) => {
   const { themeColor } = useThemeContext();
 
+  // Determine the variant of the paper
+  const variant = messagePaper && themeColor !== "dark" ? "outlined" : undefined;
+
   // Assign a default elevation and modify based on props and themeColor
-  const elevation = (messagePaper || grayVariant) && themeColor === "dark" ? 4 : 1;
+  const elevation = variant === "outlined" ? 0 : (messagePaper || grayVariant) && themeColor === "dark" ? 4 : 1;
 
   // Extend the sx prop based on conditions
   const extendedSx: SxProps = {
     ...(grayVariant && themeColor !== "dark" && { backgroundColor: "#f6f6f6" }),
-    boxShadow: 0,
+    boxShadow: "none", // Remove boxShadow if elevation is 0
     ...sx,
   };
-
-  // Determine the variant of the paper
-  const variant = messagePaper && themeColor !== "dark" ? "outlined" : undefined;
 
   return (
     <Paper sx={extendedSx} elevation={elevation} variant={variant}>
