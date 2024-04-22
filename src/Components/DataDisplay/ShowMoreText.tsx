8c4fbe72413770c4c85d9bc8cff3740d7df71c62
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-04-21 19:36:51 -0500

    ui

--- src/Components/DataDisplay/ShowMoreText.tsx
+++ src/Components/DataDisplay/ShowMoreText.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Typography, Button } from "@mui/material";
+import { useState, useRef, useEffect } from "react";
+import { Button } from "@mui/material";
 
 const MAX_HEIGHT = 190; // The maximum height of the content before "Show More" appears
 
@@ -12,28 +12,25 @@ function AboutSection({ children }) {
     if (contentRef.current) {
       setShowMoreButton(contentRef.current.clientHeight >= MAX_HEIGHT);
     }
-  }, [children]); // Check if show more button is needed when userData changes
+  }, [children]); // Check if show more button is needed when kids change
 
   const toggleExpanded = () => {
     setExpanded(!expanded);
   };
 
+  const contentStyle = {
+    maxHeight: expanded ? `${contentRef.current.scrollHeight}px` : `${MAX_HEIGHT}px`,
+    overflow: "hidden",
+    transition: "max-height 0.5s ease-in-out",
+    ...(showMoreButton && { cursor: "pointer" }), // Optional: change cursor
+  };
+
   return (
     <div>
-      <div
-        ref={contentRef}
-        style={{
-          maxHeight: expanded ? "none" : MAX_HEIGHT + "px",
-          overflow: "hidden",
-        }}
-      >
+      <div ref={contentRef} style={contentStyle}>
         {children}
       </div>
-      {showMoreButton && (
-        <>
-          <Button onClick={toggleExpanded}>{expanded ? "Show Less" : "Show More"}</Button>
-        </>
-      )}
+      {showMoreButton && <Button onClick={toggleExpanded}>{expanded ? "Show Less" : "Show More"}</Button>}
     </div>
   );
 }
