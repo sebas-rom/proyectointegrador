d18f44ce398b5c0a3008f4a9dba817ad644dcf3e
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-05-12 18:51:52 -0500

    fixed

--- src/Components/Profile/FetchProfileFeedback.tsx
+++ src/Components/Profile/FetchProfileFeedback.tsx
@@ -15,11 +15,11 @@ const FetchProfileFeedback: React.FC<ProfileFeedbackProps> = ({ uid }) => {
       const feedbackRef = collectionGroup(db, FEEDBACK_COLLECTION);
       const feedbackQuery = query(feedbackRef, where("forUser", "==", uid));
       const querySnapshot = await getDocs(feedbackQuery);
+      const tempReviews: FeedbackData[] = [];
       querySnapshot.forEach((doc) => {
-        const tempReviews: FeedbackData[] = [];
         tempReviews.push(doc.data() as FeedbackData);
-        setReviews(tempReviews);
       });
+      setReviews(tempReviews);
       setLoading(false);
     };
     fetchData();
@@ -30,14 +30,14 @@ const FetchProfileFeedback: React.FC<ProfileFeedbackProps> = ({ uid }) => {
       {loading ? (
         <>loading...</>
       ) : (
-        <>
+        <Stack spacing={2}>
           {reviews.map((review, index) => (
             <div key={index}>
               <ProfileFeedback feedbackData={review} />
             </div>
           ))}
           {reviews.length === 0 && <Typography>No reviews yet</Typography>}
-        </>
+        </Stack>
       )}
     </Stack>
   );
